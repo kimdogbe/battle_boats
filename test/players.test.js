ee1c37dbc1933461ab16e8bbe9f21590cce2df74
kimdogbe <kimdogbe@gmail.com>
2024-11-15 17:05:04 +0000

    Add player tests

--- test/players.test.js
+++ test/players.test.js
@@ -1,3 +1,15 @@
-test('pass', () => {
-  expect(1).toBe(1);
+import { createPlayer } from "../src/players";
+
+const player = createPlayer('kofi');
+
+test('check player has gameboard', () => {
+  expect(player.getGameboard()).not.toBe(undefined);
+});
+
+test('check player has name', () => {
+  expect(player.getName()).toBe('kofi');
+});
+
+test('check player win status', () => {
+  expect(player.getWinStatus()).toBe('win');
 });
